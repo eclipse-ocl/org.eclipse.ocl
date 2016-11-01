8692e0d085df57539503f2712555aa8b43508368
Ed Willink <ed@willink.me.uk>
2016-11-01 11:33:36 +0000

    [unrelated] Avoid hybrid text/XMI display.

--- plugins/org.eclipse.ocl.xtext.base.ui/src/org/eclipse/ocl/xtext/base/ui/model/BaseCSorASDocumentProvider.java
+++ plugins/org.eclipse.ocl.xtext.base.ui/src/org/eclipse/ocl/xtext/base/ui/model/BaseCSorASDocumentProvider.java
@@ -63,12 +63,12 @@ import org.eclipse.xtext.validation.IConcreteSyntaxValidator.InvalidConcreteSynt
 
 /**
  * BaseCSorASDocumentProvider orchestrates the load and saving of optional XMI content
- * externally while maintaining the serialised human friendly form internally. 
+ * externally while maintaining the serialised human friendly form internally.
  */
 public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 {
 	private static final Logger log = Logger.getLogger(BaseCSorASDocumentProvider.class);
-	
+
 	public static final String PERSIST_AS_PIVOT = "pivot";
 	public static final String PERSIST_AS_TEXT = "text";
 
@@ -277,13 +277,13 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 
 	@Override
 	protected void setDocumentContent(IDocument document, InputStream inputStream, String encoding) throws CoreException {
-//		@NonNull String displayText = sourceText;
+		//		@NonNull String displayText = sourceText;
 		try {
-//			String xmlEncoding = URIConverter.ReadableInputStream.getEncoding(sourceText);
+			//			String xmlEncoding = URIConverter.ReadableInputStream.getEncoding(sourceText);
 			if (!inputStream.markSupported()) {
 				inputStream = createResettableInputStream(inputStream);
 			}
-			boolean isXML = isXML(inputStream, encoding);		
+			boolean isXML = isXML(inputStream, encoding);
 			String persistAs = PERSIST_AS_TEXT;
 			if (isXML) {
 				ResourceSet asResourceSet = getOCL().getMetamodelManager().getASResourceSet();
@@ -292,17 +292,17 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 				projectMap.configure(asResourceSet, StandaloneProjectMap.LoadFirstStrategy.INSTANCE, conflictHandler);
 				StandaloneProjectMap.IProjectDescriptor pivotPackageDescriptor = projectMap.getProjectDescriptor(PivotConstantsInternal.PLUGIN_ID);
 				if (pivotPackageDescriptor != null) {
-					pivotPackageDescriptor.configure(asResourceSet, StandaloneProjectMap.LoadBothStrategy.INSTANCE, conflictHandler);	
+					pivotPackageDescriptor.configure(asResourceSet, StandaloneProjectMap.LoadBothStrategy.INSTANCE, conflictHandler);
 				}
 				URI uri = uriMap.get(document);
 				XMLResource xmiResource = (XMLResource) asResourceSet.getResource(uri, false);
 				if ((xmiResource == null) || (xmiResource.getResourceSet() == null)) {	// Skip built-ins and try again as a file read.
-					xmiResource = (XMLResource) asResourceSet.createResource(uri, null);					
+					xmiResource = (XMLResource) asResourceSet.createResource(uri, null);
 				}
 				else {
 					xmiResource.unload();
 				}
-//				xmiResource.load(new InputSource(new StringReader(sourceText)), null);
+				//				xmiResource.load(new InputSource(new StringReader(sourceText)), null);
 				xmiResource.load(inputStream, null);
 				EcoreUtil.resolveAll(asResourceSet);
 				List<Resource.Diagnostic> allErrors = null;
@@ -355,15 +355,15 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 				if (asResource == null) {
 					throw new CoreException(new Status(IStatus.ERROR, BaseUiModule.PLUGIN_ID, "Failed to load"));
 				}
-//				
+				//
 				ResourceSetImpl csResourceSet = (ResourceSetImpl)getOCL().getResourceSet();
 				csResourceSet.getPackageRegistry().put(PivotPackage.eNS_URI, PivotPackage.eINSTANCE);
 				URI textURI = xmiResource.getURI().appendFileExtension(getFileExtension());
 				CSResource csResource = (CSResource) csResourceSet.getResource(textURI, false);
 				if (csResource == null) {
 					csResource = (CSResource) csResourceSet.createResource(textURI, getCScontentType());
-				    Map<URI, Resource> map = csResourceSet.getURIResourceMap();
-				    map.put(textURI, csResource);
+					Map<URI, Resource> map = csResourceSet.getURIResourceMap();
+					map.put(textURI, csResource);
 					csResource.setURI(xmiResource.getURI());
 				}
 				//
@@ -374,13 +374,14 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 				csResource.updateFrom(asResource, getOCL().getEnvironmentFactory());
 				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 				inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-//				StringWriter writer = new StringWriter();
+				//				StringWriter writer = new StringWriter();
 				try {
-//					csResource.save(new URIConverter.WriteableOutputStream(writer, xmlEncoding), null);
+					//					csResource.save(new URIConverter.WriteableOutputStream(writer, xmlEncoding), null);
 					try {
 						csResource.save(outputStream, null);
 					}
 					catch (RuntimeException e) {
+						outputStream.reset();
 						Resource csResource2 = new XMIResourceFactoryImpl().createResource(csResource.getURI().appendFileExtension("xmi"));
 						csResource2.getContents().addAll(csResource.getContents());
 						csResource2.save(outputStream, null);
@@ -390,10 +391,10 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 						String message = helper.getString("_UI_SerializationFailure_message", true);
 						Status errorStatus = helper.createErrorStatus(e);
 						ErrorDialog.openError(null, title, message, errorStatus);
-//						helper.log("2" + title, errorStatus);
-//						inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-//						super.setDocumentContent(document, inputStream, encoding);
-//						throw e;
+						//						helper.log("2" + title, errorStatus);
+						//						inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+						//						super.setDocumentContent(document, inputStream, encoding);
+						//						throw e;
 					}
 					inputStream = new ByteArrayInputStream(outputStream.toByteArray());
 				} catch (InvalidConcreteSyntaxException e) {
@@ -402,37 +403,37 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 					diagnoseErrors((XtextResource) csResource, e);
 				}
 				csResource.unload();
-//				@SuppressWarnings("null")@NonNull String string = writer.toString();
-//				displayText = string;
-////				CS2ASResourceAdapter resourceAdapter = ((BaseCSResource)csResource).getCS2ASAdapter();
-////				resourceAdapter.dispose();
+				//				@SuppressWarnings("null")@NonNull String string = writer.toString();
+				//				displayText = string;
+				////				CS2ASResourceAdapter resourceAdapter = ((BaseCSResource)csResource).getCS2ASAdapter();
+				////				resourceAdapter.dispose();
 				csResourceSet.getResources().remove(csResource);
-//				inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+				//				inputStream = new ByteArrayInputStream(outputStream.toByteArray());
 			}
-//			else if (sourceText.length() <= 0) {		// Empty document
+			//			else if (sourceText.length() <= 0) {		// Empty document
 			else if (inputStream.available() == 0) {		// Empty document
 				URI uri = ClassUtil.nonNullState(uriMap.get(document));
 				Resource.Factory factory = Resource.Factory.Registry.INSTANCE.getFactory(uri);
 				if (factory instanceof OCLASResourceFactory) {
 					persistAs = PERSIST_AS_PIVOT;
 				}
-//				else if (factory instanceof UMLResourceFactoryImpl) {
-//					persistAs = PERSIST_AS_UML;
-//				}
+				//				else if (factory instanceof UMLResourceFactoryImpl) {
+				//					persistAs = PERSIST_AS_UML;
+				//				}
 				String lastSegment = uri.trimFileExtension().lastSegment();
 				if (lastSegment == null) {
 					lastSegment = "Default";
 				}
 				String testDocument = createTestDocument(uri, lastSegment);
-				inputStream = new ByteArrayInputStream(testDocument.getBytes());				
+				inputStream = new ByteArrayInputStream(testDocument.getBytes());
 			}
 			loadedAsMap.put(document, persistAs);
 			saveAsMap.put(document, persistAs);
-//		} catch (ParserException e) {
-//			throw new CoreException(new Status(IStatus.ERROR, OCLExamplesCommonPlugin.PLUGIN_ID, "Failed to load", e));
+			//		} catch (ParserException e) {
+			//			throw new CoreException(new Status(IStatus.ERROR, OCLExamplesCommonPlugin.PLUGIN_ID, "Failed to load", e));
 		} catch (IOException e) {
 			throw new CoreException(new Status(IStatus.ERROR, BaseUiModule.PLUGIN_ID, "Failed to load", e));
-/*		} catch (Throwable e) {
+			/*		} catch (Throwable e) {
 			Runnable displayRefresh = new Runnable() {
 				@Override
 				public void run() {
@@ -445,12 +446,12 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 			};
 			Display.getDefault().asyncExec(displayRefresh);
 			displayText = "/* Load failed * /";
-*/		}
-/*
- * 		This fails to setup Xtext correctly: No state leads to NPE from EcoreUtil.resolveAll.
- * 
-  		if (reload) {		
-			final InputStream finalInputStream = inputStream; 
+			 */		}
+		/*
+		 * 		This fails to setup Xtext correctly: No state leads to NPE from EcoreUtil.resolveAll.
+		 *
+  		if (reload) {
+			final InputStream finalInputStream = inputStream;
 			((XtextDocument)document).modify(new IUnitOfWork<Object, XtextResource>() {
 
 				public Object exec(XtextResource state) throws Exception {
@@ -461,8 +462,8 @@ public abstract class BaseCSorASDocumentProvider extends BaseDocumentProvider
 		}
 		else { */
 		super.setDocumentContent(document, inputStream, encoding);
-//		superSetDocumentText(document, displayText);
-//		}
+		//		superSetDocumentText(document, displayText);
+		//		}
 	}
 
 	@Override
