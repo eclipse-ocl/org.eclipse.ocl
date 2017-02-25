55138355fa7c3aa6d461b227bf390c8512c53910
Ed Willink <ed@willink.me.uk>
2017-02-25 08:47:47 +0000

    [512726] Fix CCE for navigable UML opposites

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/library/executor/LazyModelManager.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/library/executor/LazyModelManager.java
@@ -23,7 +23,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.pivot.Type;
-import org.eclipse.ocl.pivot.evaluation.ModelManager;
+import org.eclipse.ocl.pivot.evaluation.AbstractModelManager;
 import org.omg.CORBA.Environment;
 
 /**
@@ -46,73 +46,73 @@ import org.omg.CORBA.Environment;
  * </p>
  * <p>
  * See the {@link Environment} class for a description of the
- * generic type parameters of this class. 
+ * generic type parameters of this class.
  * </p>
  */
-public abstract class LazyModelManager implements ModelManager {
+public abstract class LazyModelManager extends AbstractModelManager {
 
 	private final @NonNull Map<@NonNull Type, @NonNull Set<@NonNull EObject>> modelManager = new HashMap<@NonNull Type, @NonNull Set<@NonNull EObject>>();
 	private final @NonNull Collection<@NonNull EObject> roots;
-	
+
 	/**
 	 * Initializes me with the context element of an OCL
 	 * expression evaluation.  I discover the scope of the model from this
-     * element.
-	 * 
+	 * element.
+	 *
 	 * @param context my context element
 	 */
-	public LazyModelManager(EObject context) {		
-		context = EcoreUtil.getRootContainer(context);		
+	public LazyModelManager(EObject context) {
+		context = EcoreUtil.getRootContainer(context);
 		Resource eResource = context.eResource();
-		if (eResource != null) {		
+		if (eResource != null) {
 			roots = eResource.getContents(); // the extent is the resource
-		} else {			
+		} else {
 			roots = Collections.singleton(context); // can only search this object tree
 		}
 	}
 
 	/**
 	 * Lazily computes the extent of the specified class <code>key</code>.
-	 * 
+	 *
 	 * @param type a class in the model
 	 */
 	@Override
 	public @NonNull Set<@NonNull ? extends Object> get(org.eclipse.ocl.pivot.@NonNull Class type) {
 		// TODO: Optimize by parsing ahead of time to find all EClasses that we will query
-		Set<@NonNull EObject> result = modelManager.get(type);		
+		Set<@NonNull EObject> result = modelManager.get(type);
 		if (result == null) {
 			synchronized (modelManager) {
-				result = modelManager.get(type);		
+				result = modelManager.get(type);
 				if (result == null) {
 					result = new HashSet<@NonNull EObject>();
-					modelManager.put(type, result);			
+					modelManager.put(type, result);
 					for (Iterator<EObject> iter = EcoreUtil.getAllContents(roots); iter.hasNext();) {
-						EObject next = iter.next();				
+						EObject next = iter.next();
 						if ((next != null) && isInstance(type, next)) {
 							result.add(next);
 						}
 					}
-				}		
+				}
 			}
-		}		
+		}
 		// FIXME subclasses
 		return result;
 	}
 
-    /**
-     * Implemented by subclasses to determine whether the specified element
-     * is an instance of the specified class, according to the metamodel
-     * semantics implemented by the environment that created this extent map.
-     * 
-     * @param type a class in the model
-     * @param element a potential run-time (M0) instance of that class
-     * @return <code>true</code> if this element is an instance of the given
-     *    class; <code>false</code> otherwise
-     */
+	/**
+	 * Implemented by subclasses to determine whether the specified element
+	 * is an instance of the specified class, according to the metamodel
+	 * semantics implemented by the environment that created this extent map.
+	 *
+	 * @param type a class in the model
+	 * @param element a potential run-time (M0) instance of that class
+	 * @return <code>true</code> if this element is an instance of the given
+	 *    class; <code>false</code> otherwise
+	 */
 	protected abstract boolean isInstance(@NonNull Type type, @NonNull EObject element);
-	
+
 	@Override
-    public String toString() {
+	public String toString() {
 		return modelManager.toString();
 	}
 }
