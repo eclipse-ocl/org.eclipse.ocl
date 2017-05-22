3c7c86bc6a0944a244f6f8a29038221800b0493c
Ed Willink <ed@willink.me.uk>
2017-05-22 22:05:14 +0100

    [517119] Use primitive-specific descriptors

--- examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/types/PrimitiveValueDescriptor.java
+++ examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/types/PrimitiveValueDescriptor.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2015 Willink Transformations and others.
+ * Copyright (c) 2015, 2017 Willink Transformations and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   E.D.Willink - Initial API and implementation
  *******************************************************************************/
@@ -20,7 +20,7 @@ import org.eclipse.ocl.pivot.values.Value;
 public class PrimitiveValueDescriptor extends BoxedValueDescriptor
 {
 	private @NonNull Class<?> primitiveClass;
-	
+
 	public PrimitiveValueDescriptor(@NonNull DataTypeId typeId, @NonNull Class<? extends Value> boxedClass,
 			@NonNull Class<?> primitiveClass) {
 		super(typeId, boxedClass);
@@ -29,11 +29,23 @@ public class PrimitiveValueDescriptor extends BoxedValueDescriptor
 
 	@Override
 	protected @NonNull EcoreDescriptor createEcoreDescriptor() {
+		EcoreDescriptor ecoreDescriptor = createPrimitiveObjectEcoreDescriptor(primitiveClass);
+		if (ecoreDescriptor != null) {
+			return ecoreDescriptor;
+		}
+		ecoreDescriptor = createPrimitiveTypeEcoreDescriptor(primitiveClass);
+		if (ecoreDescriptor != null) {
+			return ecoreDescriptor;
+		}
 		return new UnboxedValueDescriptor(elementId, primitiveClass);
 	}
 
 	@Override
 	protected @NonNull UnboxedDescriptor createUnboxedDescriptor() {
+		UnboxedDescriptor unboxedDescriptor = createPrimitiveTypeUnboxedDescriptor(primitiveClass);
+		if (unboxedDescriptor != null) {
+			return unboxedDescriptor;
+		}
 		return new UnboxedValueDescriptor(elementId, primitiveClass);
 	}
 }
