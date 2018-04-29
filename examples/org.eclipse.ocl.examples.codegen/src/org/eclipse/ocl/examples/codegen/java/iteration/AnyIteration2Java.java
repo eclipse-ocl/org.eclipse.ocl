25abf59fe29de2efc28b7083c1b15b5d7d570456
Ed Willink <ed@willink.me.uk>
2018-04-29 08:03:35 +0100

    [unrelated] Improve diagnostic

--- examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/iteration/AnyIteration2Java.java
+++ examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/iteration/AnyIteration2Java.java
@@ -53,7 +53,7 @@ public class AnyIteration2Java extends AbstractIteration2Java
 	public boolean appendFinalValue(@NonNull JavaStream js, @NonNull CGBuiltInIterationCallExp cgIterationCallExp) {
 		js.append("throw new ");
 		js.appendClassReference(InvalidValueException.class);
-		js.append("(\"No matching content for 'any'\");\n");
+		js.append("(\"Nothing to return for ''any''\");\n");
 		return false;
 	}
 }
