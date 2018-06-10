61c9961443d6e8e9c21b3fb4bf9a703ebf4e8d1d
Ed Willink <ed@willink.me.uk>
2018-06-10 21:51:35 +0100

    [releng] Add javadoc generation and pdfdoc promotion

--- releng/org.eclipse.ocl.releng.build-site/downloads.sh
+++ releng/org.eclipse.ocl.releng.build-site/downloads.sh
@@ -17,10 +17,16 @@
 #	PUBLISH__BUILD_T		Build type N/I/S, blank suppresses promotion
 #	PUBLISH__QUALIFIER		Version qualifier e.g. v20171020-1234
 #	PUBLISH__ALIAS			Non blank to use alias as part of final name
+#	PUBLISH__JAVADOC		The optional Javadoc zip to be published e.g. https://hudson.eclipse.org/ocl/job/ocl-photon-master/38/artifact/releng/org.eclipse.ocl.releng.build-site/target/OCL-javadoc.zip
+#	PUBLISH__PDFDOC			The optional PDF doc to be published e.g. https://hudson.eclipse.org/ocl/job/ocl-photon-master/38/artifact/releng/org.eclipse.ocl.releng.build-site/target/ocl.pdf
 #
 dropsFolder="/home/data/httpd/download.eclipse.org/modeling/mdt/ocl/downloads/drops/"
+javadocFolder="/home/data/httpd/download.eclipse.org/ocl/javadoc/"
+pdfdocFolder="/home/data/httpd/download.eclipse.org/ocl/doc/${PUBLISH__VERSION}"
 group="modeling.mdt.ocl"
 zipPrefix="mdt-ocl-Update-"
+localZip="newJavadoc.zip"
+pdfName="ocl.pdf"
 
 if [ -n "${PUBLISH__BUILD_T}" ]
 then
@@ -46,6 +52,60 @@ then
     # make sure permissions are for the intended group
     chgrp -R ${group} ${zipFile} ${zipFile}.md5 ${zipFile}.sha1
     chmod -R g+w ${zipFile} ${zipFile}.md5 ${zipFile}.sha1
+  popd
+  
+  if [ ! -d "${javadocFolder}" ]
+  then
+    mkdir ${javadocFolder}
+  fi
+  pushd ${javadocFolder}
+    curl -s -k ${PUBLISH__JAVADOC} > ${localZip}
+    if [ $? -eq 0 ]
+    then
+      javadocSize=$(wc -c <"$localZip")
+      if [ ${javadocSize} -ge 100000 ]						# A small (423 byte) file is an HTTP 404 message
+      then
+        unzip -ou ${localZip} -d new${PUBLISH__VERSION}
+        chgrp -R ${group} new${PUBLISH__VERSION}
+        chmod -R g+w new${PUBLISH__VERSION}
+        rm -rf ${localZip}
+        if [ -d "${PUBLISH__VERSION}" ]
+        then
+          mv ${PUBLISH__VERSION} old${PUBLISH__VERSION}
+          mv new${PUBLISH__VERSION} ${PUBLISH__VERSION}
+          rm -rf old${PUBLISH__VERSION}
+        else
+          mv new${PUBLISH__VERSION} ${PUBLISH__VERSION}
+        fi
+      fi
+    fi
+  popd
+  
+  if [ ! -d "${pdfdocFolder}" ]
+  then
+    mkdir ${pdfdocFolder}
+  fi
+  pushd ${pdfdocFolder}
+    curl -s -k ${PUBLISH__PDFDOC} > new${pdfName}
+    if [ $? -eq 0 ]
+    then
+      pdfSize=$(wc -c <"new${pdfName}")
+      if [ ${pdfSize} -ge 100000 ]						# A small (423 byte) file is an HTTP 404 message
+      then
+        unzip -ou ${localZip} -d new${PUBLISH__VERSION}
+        chgrp -R ${group} new${PUBLISH__VERSION}
+        chmod -R g+w new${PUBLISH__VERSION}
+        rm -rf ${localZip}
+        if [ -f "${pdfName}" ]
+        then
+          mv ${pdfName} old${pdfName}
+          mv new${pdfName} ${pdfName}
+          rm -rf old${pdfName}
+        else
+          mv new${pdfName} ${pdfName}
+        fi
+      fi
+    fi
   popd
 
 fi
