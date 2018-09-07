cef3fb42558dd92c8f1e2a32dbb4a2885ac39119
Ed Willink <ed@willink.me.uk>
2018-09-07 17:02:55 +0100

    [538771] Ensure test files use UTF-8.

--- tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/xtext/tests/JUnitStandaloneFileSystem.java
+++ tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/xtext/tests/JUnitStandaloneFileSystem.java
@@ -12,10 +12,11 @@ package org.eclipse.ocl.examples.xtext.tests;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
@@ -166,16 +167,14 @@ public class JUnitStandaloneFileSystem extends TestFileSystem
 		@Override
 		public @NonNull JUnitStandaloneTestFile getOutputFile(@NonNull String testFilePath, @NonNull InputStream inputStream) throws IOException {
 			JUnitStandaloneTestFile node = createFilePath(testFilePath);
-			if (inputStream != null) {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-				Writer writer = new FileWriter(node.getFile());
-				for (String line; (line = reader.readLine()) != null; ) {
-					writer.write(line);
-					writer.write("\n");
-				}
-				writer.close();
-				reader.close();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			Writer writer = new OutputStreamWriter(new FileOutputStream(node.getFile()), "UTF-8");
+			for (String line; (line = reader.readLine()) != null; ) {
+				writer.write(line);
+				writer.write("\n");
 			}
+			writer.close();
+			reader.close();
 			return node;
 		}
 
