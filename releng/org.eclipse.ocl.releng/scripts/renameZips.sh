a8de39c6a47846c598b28d339db45d14ad2aaaeb
Ed Willink <ed@willink.me.uk>
2018-12-05 11:46:21 +0000

    [releng] Apply copyright tool to OCL Features, Releng

--- releng/org.eclipse.ocl.releng/scripts/renameZips.sh
+++ releng/org.eclipse.ocl.releng/scripts/renameZips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #*******************************************************************************
-# Copyright (c) 2013, 2015 Willink Transformations, University of York and others.
+# Copyright (c) 2013, 2018 Willink Transformations, University of York and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v2.0
 # which accompanies this distribution, and is available at
