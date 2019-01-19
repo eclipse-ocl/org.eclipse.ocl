a32ada6e0e234d96bb01f67dc452b5af7f3a4a29
Ed Willink <ed@willink.me.uk>
2019-01-19 17:43:44 +0000

    [543599] Fix null navigation hazards

--- plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/internal/OCLExpressionImpl.java
+++ plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/internal/OCLExpressionImpl.java
@@ -338,6 +338,8 @@ implements OCLExpression {
 	 * An UnsupportedOperationException is the usual consequence of a failure to ensure that this expression's eResource()
 	 * has an EnvironmentFactoryAdapter to locate an Executor that can provide an EnvironmentFactory.
 	 *
+	 * Invoking FlowAnalysis.getFlowAnalysis directly with a known EnvironmentFactory may solve the problem.
+	 *
 	 * @since 1.3
 	 * @generated NOT
 	 */
@@ -354,6 +356,8 @@ implements OCLExpression {
 	 *
 	 * An UnsupportedOperationException is the usual consequence of a failure to ensure that this expression's eResource()
 	 * has an EnvironmentFactoryAdapter to locate an Executor that can provide an EnvironmentFactory.
+	 *
+	 * Invoking FlowAnalysis.getFlowAnalysis directly with a known EnvironmentFactory may solve the problem.
 	 *
 	 * @since 1.3
 	 * @generated NOT
