b037928d7a544feb7ee938a72b8a493009937d6e
Ed Willink <ed@willink.me.uk>
2020-01-01 17:03:43 +0000

    [unrelated] Trim bad IterableUtil

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/IterableUtil.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/IterableUtil.java
@@ -17,74 +17,20 @@ import java.util.Iterator;
 import org.eclipse.jdt.annotation.NonNull;
 
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Iterators;
 
 /**
- * IterableUtil remedies defects in com.google.common.collect.Iterables, in particular the dual Iterables such as addAll.
+ * IterableUtil remedies defects in com.google.common.collect.Iterables.
  *
  * @since 1.11
  */
 public class IterableUtil
 {
 	/**
-	 * Adds every element of one iterable to another, returning true if anything added.
-	 *
-	 * @throws UnsupportedOperationException if addTo does not support add().
-	 */
-	public <T> void addAll(@NonNull Iterable<T> addTo, @NonNull Iterable<? extends T> elementsToAdd) {
-		if (elementsToAdd instanceof Collection) {
-			if (addTo instanceof Collection) {
-				((Collection<T>)addTo).addAll((Collection<? extends T>)elementsToAdd);
-			}
-			else {
-				Iterators.addAll((Collection<T>)addTo, elementsToAdd.iterator());
-			}
-		}
-		else {
-			throw new UnsupportedOperationException();
-		}
-	}
-
-	/**
-	 * Removes every element of one iterable from another, returning true if anything removed.
-	 *
-	 * @throws UnsupportedOperationException if removeFrom does not support remove().
-	 */
-	public static boolean removeAll(@NonNull Iterable<?> removeFrom, @NonNull Iterable<?> elementsToRemove) {
-		if (elementsToRemove instanceof Collection) {
-			if (removeFrom instanceof Collection) {
-				return ((Collection<?>)removeFrom).removeAll((Collection<?>)elementsToRemove);
-			}
-			else {
-				return Iterators.removeAll(removeFrom.iterator(), (Collection<?>)elementsToRemove);
-			}
-		}
-		else {
-			Iterator<?> removeFromIterator = removeFrom.iterator();
-			boolean didRemove = false;
-			while (removeFromIterator.hasNext()) {
-				if (Iterables.contains(elementsToRemove, removeFromIterator.next())) {
-					removeFromIterator.remove();
-					didRemove = true;
-				}
-			}
-			return didRemove;
-		}
-	}
-
-	/**
 	 * Retain only those elements of one iterable that are present in another, returning true if anything removed.
-	 *
-	 * @throws UnsupportedOperationException if removeFrom does not support remove().
 	 */
-	public static boolean retainAll(@NonNull Iterable<?> removeFrom, @NonNull Iterable<?> elementsToRetain) {
+	public static boolean retainAll(@NonNull Collection<?> removeFrom, @NonNull Iterable<?> elementsToRetain) {
 		if (elementsToRetain instanceof Collection) {
-			if (removeFrom instanceof Collection) {
-				return ((Collection<?>)removeFrom).retainAll((Collection<?>)elementsToRetain);
-			}
-			else {
-				return Iterators.retainAll(removeFrom.iterator(), (Collection<?>)elementsToRetain);
-			}
+			return removeFrom.retainAll((Collection<?>)elementsToRetain);
 		}
 		else {
 			Iterator<?> removeFromIterator = removeFrom.iterator();
