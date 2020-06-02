f94b189a670ee498ffe1cc54edcbe373e1759bd8
Ed Willink <ed@willink.me.uk>
2020-06-02 13:56:38 +0100

    Fixup CodegenTests

--- tests/org.eclipse.ocl.codegen.tests/src/org/eclipse/ocl/codegen/tests/CodegenTests.java
+++ tests/org.eclipse.ocl.codegen.tests/src/org/eclipse/ocl/codegen/tests/CodegenTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Willink Transformations and others.
+ * Copyright (c) 2020 Willink Transformations and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,6 @@
  *******************************************************************************/
 package org.eclipse.ocl.codegen.tests;
 
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.ocl.examples.pivot.tests.IteratorsTest4;
-
-public class CompatibilityCodegenTests extends org.eclipse.ocl.examples.test.xtext.AllEvaluationTests
+public class CodegenTests extends org.eclipse.ocl.examples.test.xtext.AllEvaluationTests
 {
-	void dummy() {
-		@SuppressWarnings("unused")
-		@NonNull IteratorsTest4 x;
-	}
 }
