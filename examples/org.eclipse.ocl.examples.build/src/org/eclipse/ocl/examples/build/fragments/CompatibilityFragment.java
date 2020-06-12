b376309b8921eae86709ca1dafaee6524b5682eb
Ed Willink <ed@willink.me.uk>
2020-06-12 15:33:10 +0100

    [563046] Use new Xtext generator framework

--- examples/org.eclipse.ocl.examples.build/src/org/eclipse/ocl/examples/build/fragments/CompatibilityFragment.java
+++ examples/org.eclipse.ocl.examples.build/src/org/eclipse/ocl/examples/build/fragments/CompatibilityFragment.java
@@ -10,23 +10,36 @@
  *******************************************************************************/
 package org.eclipse.ocl.examples.build.fragments;
 
-import java.util.Set;
-
 import org.eclipse.xtext.Grammar;
-import org.eclipse.xtext.generator.BindFactory;
-import org.eclipse.xtext.generator.Binding;
-import org.eclipse.xtext.generator.DefaultGeneratorFragment;
 import org.eclipse.xtext.service.GrammarProvider;
+import org.eclipse.xtext.xtext.generator.AbstractXtextGeneratorFragment;
+import org.eclipse.xtext.xtext.generator.IXtextGeneratorLanguage;
+import org.eclipse.xtext.xtext.generator.model.GuiceModuleAccess;
+import org.eclipse.xtext.xtext.generator.model.TypeReference;
+
+import com.google.inject.Inject;
 
 /**
  * Provide the Xtext 2.4+ *.xtextbin support for Xtext 2.3
  */
-public class CompatibilityFragment extends DefaultGeneratorFragment
+public class CompatibilityFragment extends AbstractXtextGeneratorFragment
 {
+	@Inject
+	private Grammar grammar;
+	@Inject
+	private IXtextGeneratorLanguage language;
+
 	@Override
-	public Set<Binding> getGuiceBindingsRt(Grammar grammar) {
-		BindFactory bindFactory = new BindFactory();
-		bindFactory.addTypeToType(GrammarProvider.class.getName(), grammar.getName() + "GrammarResource.GrammarProvider");
-		return bindFactory.getBindings();
+	public void generate() {
+		GuiceModuleAccess.BindingFactory bindFactory = new GuiceModuleAccess.BindingFactory();
+		String qualifiedKeyName = GrammarProvider.class.getName();
+		String grammarName = grammar.getName();
+		int i = grammarName.lastIndexOf('.');
+		String valuePackageName = grammarName.substring(0, i);
+		String valueClassName = grammarName.substring(i+1) + "GrammarResource.GrammarProvider";
+		TypeReference keyType = new TypeReference(qualifiedKeyName);
+		TypeReference valueType = new TypeReference(valuePackageName, valueClassName);
+		bindFactory.addTypeToType(keyType, valueType);
+		bindFactory.contributeTo(language.getRuntimeGenModule());
 	}
 }
