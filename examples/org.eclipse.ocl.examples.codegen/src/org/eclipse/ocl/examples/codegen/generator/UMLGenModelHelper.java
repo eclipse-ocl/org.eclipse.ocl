3952245dd9397607bedb7d9d427da63f92b6db0b
Ed Willink <ed@willink.me.uk>
2021-02-06 09:14:11 +0000

    [570891] Resolve UML feature redefinitions

--- examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/generator/UMLGenModelHelper.java
+++ examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/generator/UMLGenModelHelper.java
@@ -10,12 +10,36 @@
  *******************************************************************************/
 package org.eclipse.ocl.examples.codegen.generator;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenFeature;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.pivot.internal.manager.PivotMetamodelManager;
+import org.eclipse.ocl.pivot.internal.utilities.PivotConstantsInternal;
 
 public class UMLGenModelHelper extends AbstractGenModelHelper
 {
 	public UMLGenModelHelper(@NonNull PivotMetamodelManager metamodelManager) {
 		super(metamodelManager);
+	}
+
+	@Override
+	public @NonNull GenFeature getGenFeature(@NonNull EStructuralFeature eStructuralFeature) throws GenModelException {
+		EStructuralFeature eFeature = resolveRedefinition(eStructuralFeature);
+		return super.getGenFeature(eFeature);
+	}
+
+	protected @NonNull EStructuralFeature resolveRedefinition(@NonNull EStructuralFeature eStructuralFeature) {
+		EStructuralFeature eFeature = eStructuralFeature;
+		for (EAnnotation eAnnotation; (eAnnotation = eFeature.getEAnnotation(PivotConstantsInternal.REDEFINES_ANNOTATION_SOURCE)) != null; ) {
+			for (EObject reference : eAnnotation.getReferences()) {
+				if (reference instanceof EStructuralFeature) {
+					eFeature = (EStructuralFeature) reference;
+					break;
+				}
+			}
+		}
+		return eFeature;
 	}
 }
