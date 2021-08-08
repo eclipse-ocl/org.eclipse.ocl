e4e20e69383f5f913bb0075463ec044428eb9dbb
Ed Willink <ed@willink.me.uk>
2021-08-08 16:10:28 +0100

    Fix @since

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/library/AbstractSimpleBinaryOperation.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/library/AbstractSimpleBinaryOperation.java
@@ -31,6 +31,9 @@ import org.eclipse.ocl.pivot.values.SymbolicValue;
  */
 public abstract class AbstractSimpleBinaryOperation extends AbstractUntypedBinaryOperation implements LibrarySimpleBinaryOperation.LibrarySimpleBinaryOperationExtension
 {
+	/**
+	 * @since 1.16
+	 */
 	protected @NonNull SymbolicValue createValidatingResultValue( @NonNull SymbolicEvaluationEnvironment evaluationEnvironment, @NonNull OperationCallExp callExp,
 			@NonNull SymbolicValue sourceSymbolicValue, @NonNull List<@NonNull SymbolicValue> argumentSymbolicValues) {
 		Operation referredOperation = PivotUtil.getReferredOperation(callExp);
