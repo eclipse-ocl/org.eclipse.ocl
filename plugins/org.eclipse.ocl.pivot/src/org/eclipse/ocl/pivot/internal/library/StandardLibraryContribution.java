b457d225eb5d10eb4dadd8d37ca566411504e911
Ed Willink <ed@willink.me.uk>
2021-09-29 12:32:42 +0100

    [575568] Add standardLibraryContribution tracing option

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/library/StandardLibraryContribution.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/library/StandardLibraryContribution.java
@@ -13,10 +13,38 @@ package org.eclipse.ocl.pivot.internal.library;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.ocl.pivot.util.PivotPlugin;
+import org.eclipse.ocl.pivot.utilities.NameUtil;
+import org.eclipse.ocl.pivot.utilities.TracingOption;
 
 public interface StandardLibraryContribution extends RegisteredContribution<@NonNull StandardLibraryContribution>
 {
-	static @NonNull Registry<@NonNull StandardLibraryContribution> REGISTRY = new AbstractRegistry<@NonNull StandardLibraryContribution>();
+	/**
+	 * @since 1.17
+	 */
+	public static final @NonNull TracingOption STANDARD_LIBRARY_CONTRIBUTION = new TracingOption(PivotPlugin.PLUGIN_ID, "standardLibraryContribution");
+
+	static @NonNull Registry<@NonNull StandardLibraryContribution> REGISTRY = new AbstractRegistry<@NonNull StandardLibraryContribution>()
+	{
+
+		@Override
+		public @Nullable StandardLibraryContribution put(@NonNull String key, @NonNull StandardLibraryContribution contribution) {
+			if (STANDARD_LIBRARY_CONTRIBUTION.isActive()) {
+				STANDARD_LIBRARY_CONTRIBUTION.println(NameUtil.debugSimpleName(this) +  ": put " + key + " => " + contribution);
+			}
+			return super.put(key, contribution);
+		}
+
+		@Override
+		public @Nullable StandardLibraryContribution remove(@NonNull String key) {
+			if (STANDARD_LIBRARY_CONTRIBUTION.isActive()) {
+				STANDARD_LIBRARY_CONTRIBUTION.println(NameUtil.debugSimpleName(this) +  ": remove " + key);
+			}
+			return super.remove(key);
+		}
+
+	};
 
 	static class Descriptor extends AbstractDescriptor<@NonNull StandardLibraryContribution> implements StandardLibraryContribution
 	{
@@ -49,16 +77,16 @@ public interface StandardLibraryContribution extends RegisteredContribution<@Non
 			return null;
 		}
 	} */
-	
+
 	/**
 	 * A Null contribution may be installed to exploit the manually loaded content
 	 * as the standard library.
 	 */
 //	static StandardLibraryContribution NULL = new Null();
-	
+
 	/**
 	 * Return the resource containing the contribution to the standard library.
-	 * 
+	 *
 	 * @return the contributing resource.
 	 */
 	@NonNull Resource getResource();
