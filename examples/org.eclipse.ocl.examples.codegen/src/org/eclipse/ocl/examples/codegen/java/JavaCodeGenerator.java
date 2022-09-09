e8ecd17f607461c3ca606ffa7d2a94d8c0b201c1
Ed Willink <ed@willink.me.uk>
2022-09-09 15:59:54 +0100

    Fix construction timing

--- examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/JavaCodeGenerator.java
+++ examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/java/JavaCodeGenerator.java
@@ -226,7 +226,6 @@ public abstract class JavaCodeGenerator extends AbstractCodeGenerator
 		javaPrimitiveNames.put(class1.getName(), class2);
 	}
 
-	protected final @NonNull CodeGenAnalyzer cgAnalyzer;
 	private /*@LazyNonNull*/ Id2EClassVisitor id2EClassVisitor = null;
 	//	protected final @NonNull Id2JavaInterfaceVisitor id2JavaInterfaceVisitor;
 	private /*@LazyNonNull*/ Id2BoxedDescriptorVisitor id2BoxedDescriptorVisitor = null;
@@ -249,7 +248,6 @@ public abstract class JavaCodeGenerator extends AbstractCodeGenerator
 
 	public JavaCodeGenerator(@NonNull EnvironmentFactoryInternal environmentFactory, @Nullable GenModel genModel) {
 		super(environmentFactory, genModel);
-		this.cgAnalyzer = createCodeGenAnalyzer();
 		BODY_NameVariant = globalNameManager.addNameVariantPrefix("BODY_");
 		IMPL_NameVariant = globalNameManager.addNameVariantPrefix("IMPL_");
 		ITER_NameVariant = globalNameManager.addNameVariantPrefix("ITER_");
@@ -292,9 +290,10 @@ public abstract class JavaCodeGenerator extends AbstractCodeGenerator
 	protected void createConstrainedOperations(@NonNull CGClass cgClass) {
 		Iterable<@NonNull Operation> constrainedOperations = getConstrainedOperations();
 		if (constrainedOperations != null) {
+			CodeGenAnalyzer analyzer2 = getAnalyzer();
 			for (@NonNull Operation constrainedOperation : constrainedOperations) {		// FIXME recurse for nested calls
 			//	CGNamedElement cgOperation = constrainedOperation.accept(as2cgVisitor);
-				CGOperation cgOperation = cgAnalyzer.createCGElement(CGOperation.class, constrainedOperation);
+				CGOperation cgOperation = analyzer2.createCGElement(CGOperation.class, constrainedOperation);
 				cgClass.getOperations().add(cgOperation);
 			}
 		}
@@ -468,11 +467,6 @@ public abstract class JavaCodeGenerator extends AbstractCodeGenerator
 		if (NameResolution.NAMES_GATHER.isActive()) {
 			NameResolution.NAMES_GATHER.println(NameUtil.debugSimpleName(cgElement) + " : " + NameUtil.debugSimpleName(nameResolution) + " in " + NameUtil.debugSimpleName(nameManager));
 		}
-	}
-
-	@Override
-	public @NonNull CodeGenAnalyzer getAnalyzer() {
-		return cgAnalyzer;
 	}
 
 	public @NonNull NameVariant getBODY_NameVariant() {
