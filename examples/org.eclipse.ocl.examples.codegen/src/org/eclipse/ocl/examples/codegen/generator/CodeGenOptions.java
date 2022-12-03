ca062de7955361c173d2242f466cbc44bc3fe476
Ed Willink <ed@willink.me.uk>
2022-12-03 06:29:38 +0000

    Promote isIncremental

--- examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/generator/CodeGenOptions.java
+++ examples/org.eclipse.ocl.examples.codegen/src/org/eclipse/ocl/examples/codegen/generator/CodeGenOptions.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
- * 
+ *
  * Contributors:
  *   E.D.Willink(CEA LIST) - Initial API and implementation
  *******************************************************************************/
@@ -15,19 +15,32 @@ package org.eclipse.ocl.examples.codegen.generator;
 public class CodeGenOptions
 {
 	private boolean useNullAnnotations = false;
-	
+
+	/**
+	 * True to generate incremental functionality.
+	 */
+	private boolean isIncremental;
+
+	public boolean isIncremental() {
+		return isIncremental;
+	}
+
+	public void setIsIncremental(boolean isIncremental) {
+		this.isIncremental = isIncremental;
+	}
+
 	public void setUseNullAnnotations(boolean useNullAnnotations) {
 		this.useNullAnnotations = useNullAnnotations;
 	}
-	
+
 	public boolean suppressNonNullWarningsForEMFCollections() {
 		return true;
 	}
-	
+
 	public boolean suppressNonNullWarningsForEMFCreates() {
 		return true;
 	}
-	
+
 	public boolean useNullAnnotations() {
 		return useNullAnnotations;
 	}
