b1012aeb95bc5aab1c8e49601c3ccc01ff955f65
Ed Willink <ed@willink.me.uk>
2024-05-30 09:30:12 +0100

    [583353] Instrument/check PivotObjectImpl.esObject

--- plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/Element.java
+++ plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/Element.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.ocl.pivot.util.Visitor;
 
 /**
@@ -131,11 +132,11 @@ public interface Element extends EObject, org.eclipse.ocl.pivot.util.Visitable {
 	@Override
 	public <R> R accept(@NonNull Visitor<R> visitor);
 
-	EObject getESObject();
+	/*@Nullable*/ EObject getESObject();
 
 	/**
 	 * @deprecated use getESObject
 	 */
 	@Deprecated
-	EObject getETarget();
+	@Nullable EObject getETarget();
 } // Element
