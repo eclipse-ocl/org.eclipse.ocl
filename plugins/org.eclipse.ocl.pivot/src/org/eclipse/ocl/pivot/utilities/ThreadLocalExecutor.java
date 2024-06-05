ff28d936c9a9f67964fdb516f3abc94734def10e
Ed Willink <ed@willink.me.uk>
2024-06-05 10:39:20 +0100

    [583353] Delay nulling environmentFactory till after detach

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/ThreadLocalExecutor.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/ThreadLocalExecutor.java
@@ -526,8 +526,9 @@ public class ThreadLocalExecutor implements Nameable
 		EnvironmentFactoryInternal oldEnvironmentFactory = this.environmentFactory;
 		if (newEnvironmentFactory != oldEnvironmentFactory) {
 			if ((oldEnvironmentFactory != null) && !oldEnvironmentFactory.isDisposed()) {
-				this.environmentFactory = null;
+			//	this.environmentFactory = null;
 				oldEnvironmentFactory.detach(this);
+				this.environmentFactory = null;
 				if (usesFinalizer) {
 				//	System.out.println(getThreadName() + " setEnvironmentFactory() gc()");
 					System.gc();
