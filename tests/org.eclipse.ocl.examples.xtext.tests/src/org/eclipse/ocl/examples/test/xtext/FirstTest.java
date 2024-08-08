353ae06faea36caed32d83564fef3e4ea1aaf9dd
Ed Willink <ed@willink.me.uk>
2024-08-08 13:15:43 +0100

    [releng] Workaround platform issue 1495

---
+++ tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/test/xtext/FirstTest.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Willink Transformations and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ocl.examples.test.xtext;
+
+import org.eclipse.ocl.examples.pivot.tests.PivotTestCase;
+import org.eclipse.ocl.xtext.idioms.ui.outline.IdiomsOutlineTreeProvider;
+
+/**
+ * The FirstTest 'suite' works around https://github.com/eclipse-platform/eclipse.platform/issues/1495 whereby
+ * non-classpath extensions points are no longer respected. THis dummy test ensures that org.eclipse.ocl.xtext.idioms.ui
+ * contributes to the classpath.
+ */
+public class FirstTest extends PivotTestCase
+{
+	public void testFirstTest() throws Exception {
+		new IdiomsOutlineTreeProvider();
+	}
+}
