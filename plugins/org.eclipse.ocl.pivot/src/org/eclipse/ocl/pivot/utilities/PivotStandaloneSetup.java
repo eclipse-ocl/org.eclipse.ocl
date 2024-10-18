df143a799ce88287d589f8c2fefb6e58343babc7
Ed Willink <ed@willink.me.uk>
2024-10-18 11:37:32 +0100

    Tidy OCLDelegateDomain

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/PivotStandaloneSetup.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/utilities/PivotStandaloneSetup.java
@@ -54,7 +54,7 @@ public class PivotStandaloneSetup //implements ISetup
 
 	public static void init() {
 		OCLDelegateDomain.lazyInitializeGlobals(PivotConstants.OCL_DELEGATE_URI_PIVOT, false);
-		OCLDelegateDomain.lazyInitializeGlobalValidationRegistry(PivotConstants.OCL_DELEGATE_URI_PIVOT_COMPLETE_OCL, false, true);
+		OCLDelegateDomain.lazyInitializeGlobalValidationRegistry(PivotConstants.OCL_DELEGATE_URI_PIVOT_COMPLETE_OCL, false);
 		OCLASResourceFactory theOCLASResourceFactory = OCLASResourceFactory.getInstance();
 		OCLstdlib.lazyInstall();
 		OCLmetamodel.lazyInstall();
