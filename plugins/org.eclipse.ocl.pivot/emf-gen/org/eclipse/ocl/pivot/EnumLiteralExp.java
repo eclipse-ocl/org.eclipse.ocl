6ab68ebe04108afd892687fedbc99ac0120a56ad
Ed Willink <ed@willink.me.uk>
2024-11-18 16:33:54 +0000

    [583353] Regen Pivot, OCLstdlib

--- plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/EnumLiteralExp.java
+++ plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/EnumLiteralExp.java
@@ -61,6 +61,9 @@ public interface EnumLiteralExp
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * The type of an enum Literal expression is the type of the referred literal.
+	 * <!-- end-model-doc -->
 	 * @generated
 	 */
 	boolean validateTypeIsEnumerationType(DiagnosticChain diagnostics, Map<Object, Object> context);
--- plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/util/PivotValidator.java
+++ plugins/org.eclipse.ocl.pivot/emf-gen/org/eclipse/ocl/pivot/util/PivotValidator.java
@@ -1835,7 +1835,7 @@ extends EObjectValidator {
 	 */
 	public boolean validatePivotable(Pivotable pivotable, DiagnosticChain diagnostics, Map<Object, Object> context)
 	{
-		return validate_EveryDefaultConstraint(pivotable, diagnostics, context);
+		return validate_EveryDefaultConstraint((EObject)pivotable, diagnostics, context);
 	}
 
 	/**
@@ -3921,7 +3921,7 @@ extends EObjectValidator {
 	 */
 	public boolean validateMorePivotable(MorePivotable morePivotable, DiagnosticChain diagnostics, Map<Object, Object> context)
 	{
-		return validate_EveryDefaultConstraint(morePivotable, diagnostics, context);
+		return validate_EveryDefaultConstraint((EObject)morePivotable, diagnostics, context);
 	}
 
 	/**
