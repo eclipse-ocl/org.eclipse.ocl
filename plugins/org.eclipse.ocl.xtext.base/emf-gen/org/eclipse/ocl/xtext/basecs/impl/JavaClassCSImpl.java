ce0c2e222b1468afa46fd8cdf9bfb0ea0f8dae7b
Ed Willink <ed@willink.me.uk>
2024-12-06 06:49:09 +0000

    Promote JavaClassCS/JavaImplementationCS ocl.xtext.base

---
+++ plugins/org.eclipse.ocl.xtext.base/emf-gen/org/eclipse/ocl/xtext/basecs/impl/JavaClassCSImpl.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2010, 2019 Willink Transformations and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   E.D.Willink - Initial API and implementation
+ */
+package org.eclipse.ocl.xtext.basecs.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.ocl.xtext.basecs.JavaClassCS;
+import org.eclipse.ocl.xtext.basecs.util.BaseCSVisitor;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Java Class CS</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class JavaClassCSImpl extends NamedElementCSImpl implements JavaClassCS
+{
+	/**
+	 * The number of structural features of the '<em>Java Class CS</em>' class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	public static final int JAVA_CLASS_CS_FEATURE_COUNT = NamedElementCSImpl.NAMED_ELEMENT_CS_FEATURE_COUNT + 0;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected JavaClassCSImpl()
+	{
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass()
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public <R> R accept(@NonNull BaseCSVisitor<R> visitor) {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(name);
+	}
+} //JavaClassCSImpl
