bde6ae5eadc3a3e1f9b7cf1de88042c2b2b0035f
Ed Willink <ed@willink.me.uk>
2024-12-08 13:59:56 +0000

    [583176] Improved EnvironmentFactory discovery / passing

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/utilities/PivotUtilInternal.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/utilities/PivotUtilInternal.java
@@ -127,12 +127,12 @@ public class PivotUtilInternal //extends PivotUtil
 	/**
 	 * @since 1.24
 	 */
-	public static @Nullable EnvironmentFactoryInternal basicGetEnvironmentFactory(@Nullable Notifier notifier) {
+	public static @Nullable EnvironmentFactoryInternal basicGetEnvironmentFactory(@Nullable Object object) {
 		EnvironmentFactoryInternal environmentFactory = ThreadLocalExecutor.basicGetEnvironmentFactory();
 		if (environmentFactory != null) {
 			return environmentFactory;
 		}
-		ResourceSet resourceSet = PivotUtil.basicGetResourceSet(notifier);
+		ResourceSet resourceSet = PivotUtil.basicGetResourceSet(object);
 		if (resourceSet != null) {		// null if working with installed resources
 			EnvironmentFactoryAdapter environmentFactoryAdapter = EnvironmentFactoryAdapter.find(resourceSet);
 			if (environmentFactoryAdapter != null) {
