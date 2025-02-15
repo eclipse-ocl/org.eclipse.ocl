f2570b42c75398b5ca141753d43216b10cbbdd81
Ed Willink <ed@willink.me.uk>
2025-02-15 17:26:52 +0000

    [583353] Add missing createCS2AS

--- plugins/org.eclipse.ocl.xtext.oclstdlib/src/org/eclipse/ocl/xtext/oclstdlib/utilities/OCLstdlibASResourceFactory.java
+++ plugins/org.eclipse.ocl.xtext.oclstdlib/src/org/eclipse/ocl/xtext/oclstdlib/utilities/OCLstdlibASResourceFactory.java
@@ -16,9 +16,13 @@ import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.ocl.pivot.internal.resource.ASResourceFactory;
 import org.eclipse.ocl.pivot.internal.resource.AbstractASResourceFactory;
+import org.eclipse.ocl.pivot.internal.resource.ICS2AS;
+import org.eclipse.ocl.pivot.internal.utilities.EnvironmentFactoryInternal;
 import org.eclipse.ocl.pivot.resource.ASResource;
+import org.eclipse.ocl.pivot.resource.CSResource;
 import org.eclipse.ocl.pivot.resource.NotXMLContentHandlerImpl;
 import org.eclipse.ocl.pivot.utilities.PivotConstants;
+import org.eclipse.ocl.xtext.oclstdlib.cs2as.OCLstdlibCS2AS;
 
 public final class OCLstdlibASResourceFactory extends AbstractASResourceFactory
 {
@@ -49,6 +53,11 @@ public final class OCLstdlibASResourceFactory extends AbstractASResourceFactory
 
 	public OCLstdlibASResourceFactory() {
 		super(ASResource.OCLSTDLIB_CONTENT_TYPE, null);
+	}
+
+	@Override
+	public @NonNull ICS2AS createCS2AS(@NonNull EnvironmentFactoryInternal environmentFactory, @NonNull CSResource csResource, @NonNull ASResource asResource) {
+		return new OCLstdlibCS2AS(environmentFactory, csResource, asResource);
 	}
 
 	@Override
