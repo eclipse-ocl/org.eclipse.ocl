050cbcaf551672bfc504e7e8bf450a56fd03d8e9
Ed Willink <ed@willink.me.uk>
2025-02-15 17:10:32 +0000

    Fixup AbstractOCLDelegateFactory

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/delegate/AbstractOCLDelegateFactory.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/internal/delegate/AbstractOCLDelegateFactory.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
- * 
+ *
  * Contributors:
  *   C.Damus, K.Hussey, E.D.Willink - Initial API and implementation
  *   E.D.Willink - Bug 353171
@@ -59,7 +59,7 @@ public abstract class AbstractOCLDelegateFactory
 	}
 
 	/**
-	 * Return the DelegateDomain for this package, creating one if it does not already exist. 
+	 * Return the DelegateDomain for this package, creating one if it does not already exist.
 	 */
 	protected OCLDelegateDomain loadDelegateDomain(@NonNull EPackage ePackage) {
 		DelegateEPackageAdapter ePackageAdapter = DelegateEPackageAdapter.getAdapter(ePackage);
