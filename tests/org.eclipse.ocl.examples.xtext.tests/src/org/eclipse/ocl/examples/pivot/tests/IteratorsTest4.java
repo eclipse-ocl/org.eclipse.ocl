2c9d11b8d0157b602cbe1c82069389d2d5fcb6f1
Ed Willink <ed@willink.me.uk>
2025-03-14 12:28:23 +0000

    search tests pass non-CG

--- tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/pivot/tests/IteratorsTest4.java
+++ tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/pivot/tests/IteratorsTest4.java
@@ -1088,7 +1088,8 @@ public class IteratorsTest4 extends PivotTestSuite
 		MyOCL ocl = createOCL();
 	//	ocl.assertQueryTrue(ocl.pkg1, "Sequence{'a', 'b', 'c', 'd', 'e'}->select(p | p.charAt(p))");
 //		ocl.assertSemanticErrorQuery(ocl.pkg1, "Sequence{'a', 'b', 'c', 'd', 'e'}->select(5)");
-		ocl.assertQueryTrue(ocl.pkg1, "Sequence{'a', 'b', 'c', 'd', 'e'}->search(e; acc : Integer = 0 | e = 'c', acc < 2, acc = 1)");
+		ocl.assertSemanticErrorQuery(ocl.getContextType(ocl.pkg1), "Sequence{'a', 'b', 'c', 'd', 'e'}->search(e; acc : Integer = 0 | e = 'c', acc < 2, acc = 1)",
+			PivotMessages.ExpectedAccumulatorType, "search", "OclAny", "Integer");
 		ocl.assertQueryTrue(ocl.pkg1, "Sequence{'a', 'b', 'c', 'd', 'e'}->search(e; acc : Integer = 0 | if e = 'c' then acc+1 else acc endif, acc < 2, acc = 1)");
 
 /*		ocl.assertQueryFalse(ocl.pkg1, "Sequence{'a', 'b', 'c', 'c', 'e'}->one(e | e = 'c')");
