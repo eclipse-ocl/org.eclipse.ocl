9429e02a8dd4ae11bf8c80925fa0871e457e9b6f
Ed Willink <ed@willink.me.uk>
2025-05-13 21:50:22 +0100

    [#2285] Eliminate the dead WeakHashMapOf...WeakReference usage

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/ids/BindingsId.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/ids/BindingsId.java
@@ -10,22 +10,17 @@
  *******************************************************************************/
 package org.eclipse.ocl.pivot.ids;
 
-import java.util.Iterator;
-
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.ocl.pivot.utilities.IndexableIterable;
 
 /**
  * BindingsId provides a hashable list of Class-typed elementIds and/or DataType-typed values that parameterize a template specialization.
  */
-public interface BindingsId extends IndexableIterable<@NonNull ElementId>	// Use of IndexableIterable is deprecated
+public interface BindingsId
 {
 	/**
 	 * @since 1.18
 	 */
 	int elementIdSize();
-	@Override @Deprecated
-	@NonNull ElementId get(int index);
 	/**
 	 * @since 1.18
 	 */
@@ -34,10 +29,6 @@ public interface BindingsId extends IndexableIterable<@NonNull ElementId>	// Use
 	 * @since 1.18
 	 */
 	@NonNull Object getValue(int i);
-	@Override @Deprecated
-	@NonNull Iterator<@NonNull ElementId> iterator();
-	@Override @Deprecated
-	int size();
 	/**
 	 * @since 1.18
 	 */
