531253787cac0992391d01b2bc441c935c2065cd
Ed Willink <ed@willink.me.uk>
2026-04-27 11:25:52 +0100

    Fixup [#2285] Eliminate WeakOCLReference, PivotDiagnostician.getEnvironmentFactory

--- plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/validation/ValidationContext.java
+++ plugins/org.eclipse.ocl.pivot/src/org/eclipse/ocl/pivot/validation/ValidationContext.java
@@ -17,14 +17,23 @@ import java.util.Map;
 
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.ocl.pivot.internal.resource.ASResourceFactoryRegistry;
 import org.eclipse.ocl.pivot.internal.utilities.EnvironmentFactoryInternal;
+import org.eclipse.ocl.pivot.resource.BasicProjectManager;
 import org.eclipse.ocl.pivot.utilities.EnvironmentFactory;
 import org.eclipse.ocl.pivot.utilities.LabelUtil;
 import org.eclipse.ocl.pivot.utilities.PivotUtil;
+import org.eclipse.ocl.pivot.utilities.ThreadLocalExecutor;
+import org.eclipse.ocl.pivot.utilities.ThreadLocalExecutor.InitWrapperCallBack;
 
 /**
  * ValidationContext reifies the Map<Object,Object> context argument for the EValidator API.
@@ -56,30 +65,84 @@ public class ValidationContext extends HashMap<Object,Object>
 	}
 
 	/**
-	 * Return the current EnvironmentFactory preferably from the cached validationContext entry, else falling back on PivotUtil.getEnvironmentFactory(Notifier)
+	 * Return the current EnvironmentFactory preferably from the cached validationContext entry,
+	 * else falling back on a cached/created AdapterFactoryEditingDomainResourceSet's ThreadLOcalExecutor cache
+	 * else falling back on PivotUtil.getEnvironmentFactory(Notifier)
 	 * and caching the result for subsequent use.
 	 *
 	 * @since 7.0
 	 */
 	public static @NonNull EnvironmentFactoryInternal getEnvironmentFactory(Map<Object, Object> validationContext, @Nullable Object object) {
+		//
+		//	If already cached by validationContext, re-use.
+		//
 		if (validationContext != null) {
 			Object environmentFactory = validationContext.get(EnvironmentFactory.class);
 			if (environmentFactory != null) {
 				return (EnvironmentFactoryInternal)environmentFactory;
 			}
 		}
-		EnvironmentFactoryInternal environmentFactory;
-		if (object instanceof Notifier) {
-			environmentFactory = PivotUtil.getEnvironmentFactory((Notifier)object);
-			if (validationContext != null) {
-				validationContext.put(EnvironmentFactory.class, environmentFactory);
+		//
+		//	If already cached by the ThreadLocalExecutor, re-use.
+		//
+		EnvironmentFactoryInternal environmentFactory = PivotUtil.basicGetEnvironmentFactory(object);
+		if (environmentFactory != null) {
+			return environmentFactory;
+		}
+		//
+		//	If validating probably as part of a GenModelEditor's AdapterFactoryEditingDomainResourceSet,
+		//	re-use/create the per-EditingDomain EnvironmentFactoryInternal cached by the ThreadLocalExecutor.
+		//
+		if (object instanceof EObject) {
+			EObject eObject = (EObject)object;
+			Resource eResource = eObject.eResource();
+			if (eResource != null) {
+				final ResourceSet resourceSet = eResource.getResourceSet();
+				if (resourceSet instanceof IEditingDomainProvider) {			// AdapterFactoryEditingDomainResourceSet
+					EditingDomain editingDomain = ((IEditingDomainProvider)resourceSet).getEditingDomain();
+					assert editingDomain != null;
+					InitWrapperCallBack<@NonNull EnvironmentFactoryInternal, @Nullable Object> callBack = new InitWrapperCallBack<@NonNull EnvironmentFactoryInternal, @Nullable Object>()
+					{
+						protected @Nullable EnvironmentFactoryInternal environmentFactoryInternal = null;
+
+						@Override
+						public @NonNull EnvironmentFactoryInternal getResult() {
+							assert environmentFactoryInternal != null;
+							return environmentFactoryInternal;
+						}
+
+						@Override
+						public void run() {
+							ResourceSet resourceSet2 = editingDomain.getResourceSet();
+							assert resourceSet2 == resourceSet;		// XXX debugging
+							environmentFactoryInternal = ASResourceFactoryRegistry.INSTANCE.createEnvironmentFactory(BasicProjectManager.createDefaultProjectManager(), resourceSet2, null);
+						}
+					};
+					ThreadLocalExecutor.init(editingDomain, callBack);
+					environmentFactory = callBack.getResult();
+					ThreadLocalExecutor.setUsesFinalizer();			// XXX not on a worker thread ?? part threads may also use fiunalizer ??
+				}
 			}
 		}
-		else {
-			// In the unlikely (? impossible) event that an EDataType validation occurs before an EClass validation caches an appropriate
-			//  ResourceSet-specific EnvironmentFactory, use but do not cache a global EnvironmentFactory.
-			System.out.println("EDataType validation without prior eClass validation");
-			environmentFactory = PivotUtil.getEnvironmentFactory(null);
+		//
+		//	Create an EnvironmentFactoryInternal cached by the ThreadLocalExecutor.
+		//
+		if (environmentFactory == null) {
+			if (object instanceof Notifier) {
+				environmentFactory = PivotUtil.getEnvironmentFactory((Notifier)object);
+			}
+			else {
+				// In the unlikely (? impossible) event that an EDataType validation occurs before an EClass validation caches an appropriate
+				//  ResourceSet-specific EnvironmentFactory, use but do not cache a global EnvironmentFactory.
+				System.out.println("EDataType validation without prior eClass validation");
+				environmentFactory = PivotUtil.getEnvironmentFactory(null);
+			}
+		}
+		//
+		//	Cache it in the ValidationContext
+		//
+		if (validationContext != null) {
+			validationContext.put(EnvironmentFactory.class, environmentFactory);
 		}
 		return environmentFactory;
 	}
