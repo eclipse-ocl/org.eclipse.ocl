35339637ed268ad91ef2ff3eea610fd8a7826a6f
Ed Willink <ed@willink.me.uk>
2026-04-30 16:59:29 +0100

    Trim debug

--- plugins/org.eclipse.ocl.xtext.base/src/org/eclipse/ocl/xtext/base/utilities/BaseCSXMIResource.java
+++ plugins/org.eclipse.ocl.xtext.base/src/org/eclipse/ocl/xtext/base/utilities/BaseCSXMIResource.java
@@ -79,7 +79,7 @@ public abstract class BaseCSXMIResource extends XMIResourceImpl implements CSRes
 		@Override
 		public String getHREF(EObject obj) {
 			String href2 = getHREF2(obj);
-		System.out.println(obj + " => " + href2);
+	//	System.out.println(obj + " => " + href2);
 		if (href2.contains("Pivot.oclas")) {
 			href2 = getHREF2(obj);
 			getClass();			// XXX
